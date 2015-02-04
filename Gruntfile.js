26546c54c81478e919278430bd1898a6b8451188
David Morrow <dave@wikihow.com>
2015-02-04 09:52:06 -0800

    initial commit of files for project

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jslint: {
+      library: {
+        src: [
+          'lib/*.js'
+        ],
+        directives: {
+          browser: true,
+          predef: ['window'],
+          indent: 2,
+          regexp: true
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: "/*\n<%= pkg.name %>\nversion: <%= pkg.version %>\ncompiled: <%= grunt.template.today('yyyy-mm-dd') %>\n<%= pkg.author %>\n*/",
+        mangle: false,
+        sourceMap: true
+      },
+      my_target: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['lib/defer-images.js']
+        }
+      }
+    },
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-jslint'); // load the task
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', 'jslint');
+};
